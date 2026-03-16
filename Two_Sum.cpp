91e0051dcd6ba812d09a46f3cff2e5812270e1cf
ShashankChaturvedi07 <shashankc585@gmail.com>
2026-03-17 00:12:50 +0530

    Added Optimal (two pointer) Solution

--- Two_Sum.cpp
+++ Two_Sum.cpp
@@ -1,24 +1,53 @@
 
             //BRUTE 
 
+// #include <iostream>
+// using namespace std;
+
+// int main(){
+
+//     int arr[5]= {2,5,6,8,9};
+//     int tar=70;
+
+//     for(int i=0; i<5; i++){
+//         for(int j=0; j<5; j++){
+//             if(arr[i]+arr[j] == tar){
+//                 cout<<arr[i]<<","<<arr[j];
+//             }
+//         }
+//     }
+
+//     cout<<"Not found";
+
+
+//     return 0;
+// }
+        //BETTER (use hashmap)
+
+        //Optimal  (TC-O(N)  SC-O(1))
+
 #include <iostream>
 using namespace std;
 
 int main(){
+    int arr[]={1,2,5,7,9};
+    int n=5;
+    int tar=12;
+    int i=0;
+    int j=n-1;
 
-    int arr[5]= {2,5,6,8,9};
-    int tar=70;
-
-    for(int i=0; i<5; i++){
-        for(int j=0; j<5; j++){
-            if(arr[i]+arr[j] == tar){
-                cout<<arr[i]<<","<<arr[j];
-            }
+    while(i<j){
+        int sum = arr[i]+arr[j];
+        if(sum==tar){
+            cout<<arr[i]<<","<<arr[j];
+            return 0;
+        }
+        else if(sum>tar){
+            j--;
+        }
+        else{
+            i++;
         }
     }
-
-    cout<<"Not found";
-
-
-    return 0;
+    cout<<"not found";
 }
